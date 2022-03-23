e7d50a02ef9a4337eb6f76aac564c8fc9f11e5bd
DaveRoCa <s212767@student.dtu.dk>
2022-03-23 21:46:47 +0000

    Final function 2 (DNA to RNA)

--- R/dna_to_rna.R
+++ R/dna_to_rna.R
@@ -1,7 +1,7 @@
-#' dna_to_rna
+#' Convert DNA to RNA
 #'
 #' @description
-#' Convert a DNA sequence into a RNA sequence by replacing the Thymines (T) in
+#' Convert a DNA sequence into a RNA sequence by replacing the Thymines (T) by
 #' Uracil (U) present in the given sequence.
 #' @param dna_seq The DNA sequence that is going to be converted into RNA.
 #'
@@ -11,7 +11,7 @@
 #' @examples
 #' dna_seq = "AGAGTTTGATCCTGGCTCAG"
 #' dna_to_rna(dna_seq)
-rna_seq <- function(dna_seq){
-  converted_seq <- gsub("T", "U", dna_seq)
-  return(converted_seq)
+dna_to_rna <- function(dna_seq){
+  rna_seq <- gsub("T", "U", dna_seq)
+  return(rna_seq)
 }

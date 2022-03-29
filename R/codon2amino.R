673c92b3cb634ac0a9ededf0ba5c9cb1298b9fce
Mathias <mnstokholm@gmail.com>
2022-03-29 11:23:46 +0000

    ...

--- R/codon2amino.R
+++ R/codon2amino.R
@@ -1,12 +1,10 @@
 #' Title: Using the codon table identify the codons and their respective amino acid, return the aminoacids
 #'
 #' @param codons codons from F3
-#' @param codon_table codon_table
 #' @return amino_seq 1 letter code AA
 #' @export
 #'
 codon2amino<- function(codons){
-  data("codon_table")
   amino_seq <- paste0(codon_table[codons], collapse = "")
   return(amino_seq)
 }
